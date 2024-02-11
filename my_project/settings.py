d951b7d70987ffe76379ea1aa9f3e94cc7af30ad
tineggarcia <tineggarcia@gmail.com>
2024-02-11 16:19:33 +0000

    Add custom user model and test

--- my_project/settings.py
+++ my_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.CustomUser"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
