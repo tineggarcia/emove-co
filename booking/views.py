eb87d440b217c61c4f613d8354de59c2a588d0fd
tineggarcia <tineggarcia@gmail.com>
2024-02-11 21:36:24 +0000

    Add success/error messages after save of Booking

--- booking/views.py
+++ booking/views.py
@@ -1,48 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib import messages
 from .models import Booking
 from .forms import BookingForm
-
-# dummy booking
-# bookings = [
-#     {
-#         'booking_id':'1',
-#         'name':'Tine Ganda',
-#         'from':'HWR Avenue, D15',
-#         'to':'Tyrellstown, D15',
-#         'date_created':'01/02/2024',
-#         'item_type':'Large Box 30x30x30',
-#         'description':'Bowling Ball',
-#         'price':'10',
-#         'status':'Accepted',
-#         'driver':'Baby Driver'
-#     },
-#     {
-#         'booking_id':'2',
-#         'name':'Jade Pretty',
-#         'from':'HWR Avenue, D15',
-#         'to':'Bay Meadows, D15',
-#         'date_created':'01/02/2024',
-#         'item_type':'Large Box 30x30x30',
-#         'description':'Basket Ball',
-#         'price':'5',
-#         'status':'Completed',
-#         'driver':'Baby Driver'
-#     },
-#     {
-#         'booking_id':'3',
-#         'name':'Jared Pogi',
-#         'from':'HWR Avenue, D15',
-#         'to':'Huntstown, D15',
-#         'date_created':'02/02/2024',
-#         'item_type':'Small Box 10x10x10',
-#         'description':'Tennis Ball',
-#         'price':'3',
-#         'status':'Pending',
-#         'driver':''
-#     },
-# ]
 
 
 def home(request):
@@ -66,8 +26,9 @@ def create_booking(request):
         form = BookingForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Booking successfully created.')
             return redirect('bookings')
         else:
+            messages.error(request, 'Please correct the following errors:')
             return render(request, 'booking/booking_form.html', {'form': form})
-    # return HttpResponse('<h1>Create Booking Here</h1>')
 
