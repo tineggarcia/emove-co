28ec177155bc55e2356d35ce2db5a0336a55233a
tineggarcia <tineggarcia@gmail.com>
2024-02-11 23:12:27 +0000

    New functionality - User Login Issue #9

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 
 from .models import CustomUser
 
@@ -14,3 +15,7 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email","first_name","last_name")
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(max_length=254)
+    password = forms.CharField(max_length=128, widget=forms.PasswordInput)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from .forms import LoginForm
 
-# Create your views here.
+
+def sign_in(request):
+
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request,'users/login.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request,email=email,password=password)
+            if user:
+                login(request, user)
+                messages.success(request,f'Hi {user.first_name} {user.last_name}, welcome back!')
+                return redirect('bookings')
+
+        # form is not valid or user is not authenticated
+        messages.error(request,f'Invalid username or password')
+        return render(request,'users/login.html',{'form': form})
