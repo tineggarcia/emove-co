1a0df4f1406c7062dbc89b79ab5c0b4133e7b04b
tineggarcia <tineggarcia@gmail.com>
2024-02-12 03:22:54 +0000

    Create Navbar and design landing page. Issue #10 and #12

--- booking/urls.py
+++ booking/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='bookings'),
-    path('about/', views.about, name='about'),
+    path('', views.about, name='about'),
+    path('bookings/', views.home, name='bookings'),
+    # path('about/', views.about, name='about'),
     path('booking/create/', views.create_booking, name='booking-create'),
     path('booking/edit/<int:id>/', views.edit_booking, name='booking-edit'),
     path('booking/delete/<int:id>/', views.delete_booking, name='booking-delete'),
